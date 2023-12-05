9b082d84ba74778128be770d0374652401dfb43a
lucianosants <lucianosantos.developer@gmail.com>
2023-12-05 09:41:42 -0300

    feat(init): implements shadcn-ui and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { ThemeProvider } from './components/theme-provider';
+
 function App() {
     return (
-        <>
-            <div>Enrollment List</div>
-        </>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+            <div className="container flex flex-col max-w-6xl min-h-screen py-4 mx-auto">
+                Enrollment List
+            </div>
+        </ThemeProvider>
     );
 }
 
